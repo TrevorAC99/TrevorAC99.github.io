09d94c6e33c4158a27bb93cac9d9ebe3258b04fc
Trevor Carlson <trevorac99@gmail.com>
2019-12-19 21:30:12 -0600

    style

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver,
-         Breakpoints } from '@angular/cdk/layout';
+import {
+  BreakpointObserver,
+  Breakpoints
+} from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 
@@ -11,12 +13,13 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainComponent {
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches),
-      shareReplay()
-    );
+  isHandset$: Observable<boolean> =
+    this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
