0fb6e751c50a8b7dd4dddda14c9e10f13e7544fa
Trevor Carlson <trevorac99@gmail.com>
2019-12-19 23:00:50 -0600

    feat: add information to the about tab

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -7,6 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public aboutMe = 'I\'m Trevor Carlson, a 3rd year student ' +
+    'at the University of Wisconsin Oshkosh studying Computer ' +
+    'Science with an emphasis in Systems and Theory. I do web ' +
+    'development work for the University, but as my emphasis ' +
+    'suggests I have a lot of interests in systems programming.';
+
+  public technologies = [
+    'Java',
+    'C#',
+    'ASP.NET',
+    'Microsoft SQL Server',
+    'MySQL',
+    'JavaScript',
+    'TypeScript',
+    'Angular',
+    'Rust'
+  ];
+
+  public info = [
+    {
+      label: 'Email',
+      value: 'trevorac99@gmail.com'
+    },
+    {
+      label: 'Phone Number',
+      value: '(920) 229-0865'
+    },
+    {
+      label: 'LinkedIn',
+      value: 'https://www.linkedin.com/in/trevor-a-carlson/'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
